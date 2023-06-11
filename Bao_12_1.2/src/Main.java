c018abd57700bce8d6c51c7b635824c37bea42ba
Thế Bảo <121713037+Baohamchoi@users.noreply.github.com>
2023-06-11 17:25:08 +0700

    Update Main.java

--- Bao_12_1.2/src/Main.java
+++ Bao_12_1.2/src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-// Review by dvhoang
+// Review by dvhoang v2
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
